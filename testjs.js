4f9b270df4901a39b3b54a18d6ef97c9c5c93b07
Nathan Guillaumin <nathan.guillaumin@outlook.com>
2020-03-26 10:25:20 +0100

    intégration fonctionnalités JS

---
+++ testjs.js
@@ -0,0 +1,29 @@
+//Fitness rooms paragraph
+document.getElementById("toKnowFitness").addEventListener("click", function(){
+  document.getElementById("textToUploadFitness").innerHTML = "Nouveau dans la capitale des Gaules, ou simplement enfin décidé à vous remettre au sport ? A Lyon, vous n'aurez pas de soucis pour trouver la salle de sport adéquate alliant appareils de fitness, cours en groupe (yoga, renforcement, arts martiaux...) et détente, et ce quel que soit votre arrondissement, ou votre quartier.  Que vous soyez résident de la Presqu’Île, ou que vous viviez plutôt sur les Pentes de la Croix Rousse, ou encore, que vous travailliez près de la Part Dieu ou de Confluence, de nombreux clubs vous accueillent et vous permettent de vivre l’expérience fitness de votre choix.";
+});
+
+//Martial arts paragraph
+document.getElementById("toKnowMartialArts").addEventListener("click", function(){
+  document.getElementById("textToUploadMartialArts").innerHTML = "Lyon ne manque pas de clubs et de salles d’arts martiaux et votre choix devra certainement se faire selon votre niveau et votre expérience. En premier lieu, nous citerons le Club du Rhône qui a su évoluer en ajoutant un large éventail de cours d’arts martiaux tels que Jeet Kun Do, des boxes, du MMA pour les plus motivés et du self-défense pour apprendre à se défendre. Vous êtes plutôt expert et souhaiter vous perfectionner en Kung-Fu, Kick-Boxing ou Sanda-Jujitsu, venez vous inscrire au club Hungsing Kungfu !";
+});
+
+//Climbing paragraph
+document.getElementById("toKnowClimbing").addEventListener("click", function(){
+  document.getElementById("textToUploadClimbing").innerHTML = "Le nouveau lyonnais que vous êtes sera censé savoir que la ville est situé des Alpes à proximité de nombreux lieux dont sont friands les experts en escalade. Commençant par le département de l’Ain et ses falaises du Bugey, nous pouvons également citer le Vercors et plus particulièrement Presles, ce serait peut-être une bonne idée de se mettre à l’escalade ! Et pour cela, Lyon compte pas moins de trois salles d'escalade réparties sur toute l'agglomération qui rassemble un large public de grimpeurs. Avec des salles de 1000 (MRoc) à 2400m2 (Climb Up Gerland), nous vous promettons que vous vous régalerez.";
+});
+
+//Running paragraph
+document.getElementById("toKnowRunning").addEventListener("click", function(){
+  document.getElementById("textToUploadRunning").innerHTML = "Avec des collines et des grands parcs, ainsi que des espaces naturels proches de la ville, Lyon offre aux joggeurs amateurs ou confirmés de multiples possibilités de pratiquer leur activité préférée. Si le Parc de la Tête d'Or -et son tour de 3.8 km- reste le lieu privilégié des joggers du centre-ville, de nombreux coureurs lyonnais empruntent régulièrement les berges du Rhône ou de Saône. Dans l'agglomération, d'autres parcs permettent également de courir en plein air, citons par exemple le Parc de Parilly, celui de la Feyssine ou de Lacroix-Laval.";
+});
+
+//Team sports
+document.getElementById("toKnowTeamSports").addEventListener("click", function(){
+  document.getElementById("textToUploadTeamSports").innerHTML = "Vous aimez la pratique du sport en groupe et les sports collectifs ? Cela tombe bien, à Lyon vous allez être servi. Quel que soit votre niveau, les clubs de quartier sont nombreux. En football, le Football Croix Roussien, le Football Club de Gerland ou l’Association Sportive de Montchat pourront vous accueillir. En rugby, l’EMS Bron, le LOU, ou le Rugby CO sont parmis les dizaines de club présent sur l’agglomération. Pour tous les autres sports, cliquez ici.";
+});
+
+//Sports and hiking grounds
+document.getElementById("toKnowSportsGround").addEventListener("click", function(){
+  document.getElementById("textToUploadSportsGround").innerHTML = "La ville gère et entretient une cinquantaine de terrains de proximité, principalement dédié aux sports collectifs. Elle est également à l'origine de plusieurs skate parcs, réservés aux adeptes de la glisse urbaine sous toutes ses formes (trottinettes, rollers, skate, BMX...).";
+});
